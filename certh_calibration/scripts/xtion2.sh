b1c727045187d1e82e0007e6c6e2d8653687ead9
akargakos <andreaskargakos@gmail.com>
2013-08-29 16:41:47 +0300

    t-shirt added

--- certh_calibration/scripts/xtion2.sh
+++ certh_calibration/scripts/xtion2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rosrun certh_calibration handeye_calibration --camera xtion2 --arm r2 --bbox -0.2 -1.2 0.0 0.2 -0.8 0.4 --orient 0 0 -1 --board 7 5 --cell 0.04
+rosrun certh_calibration handeye_calibration --camera xtion2 --arm r2 --bbox -0.2 -1.4 0.0 0.2 -1.2 0.4 --orient 0 0 -1 --board 7 5 --cell 0.04
