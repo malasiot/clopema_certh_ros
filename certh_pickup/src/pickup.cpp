104df61a94c755a958496be2a75fa7bbef5cc692
akargakos <andreaskargakos@gmail.com>
2013-08-30 15:53:08 +0300

    fixed sensor planning

--- certh_pickup/src/pickup.cpp
+++ certh_pickup/src/pickup.cpp
@@ -2,6 +2,7 @@
 
 #include <robot_helpers/Robot.h>
 #include <robot_helpers/Utils.h>
+#include <robot_helpers/Geometry.h>
 
 #include <camera_helpers/OpenNICapture.h>
 #include <pcl/io/pcd_io.h>
@@ -22,9 +23,10 @@ using namespace std;
 using namespace robot_helpers ;
 using namespace Eigen ;
 
-const double defaultDist = 0.9 ;
+const double defaultDist = 1.0 ;
 const double defaultTableHeight = 0.75 ;
-const double gripperOffset = 0.2 ;
+//const double defaultTableHeight = 0.075 ;
+const double gripperOffset = 0.35 ;
 
 
 Affine3d getSensorPose(const tf::TransformListener &listener, const string &camera)
@@ -50,6 +52,8 @@ double getTableHeightFromPlane(const Vector3d &n, double d, const Affine3d &fram
     return n.dot(frame.translation()) + d ;
 }
 
+cv::Mat clr_ ;
+
 Vector3d retargetSensor(const cv::Mat dmap, const vector<cv::Point> &hull, const image_geometry::PinholeCameraModel &cm, double Zd)
 {
     double minZ, maxZ, Z ;
@@ -68,11 +72,17 @@ Vector3d retargetSensor(const cv::Mat dmap, const vector<cv::Point> &hull, const
 
     double zS = Zd * rad / std::min(dmap.cols, dmap.rows) ;
 
+    cv::circle(clr_, center, rad, cv::Scalar(255, 0, 255)) ;
+
     zS = std::max(minZ, zS) ;
+
+    zS = 0.7 + maxZ/1000 ;
 
     cv::Point3d p = cm.projectPixelTo3dRay(cv::Point2d(center.x, center.y));
 
     p.x *= zS ; p.y *= zS ; p.z *= zS ;
+
+    cv::imwrite("/tmp/oo.png", clr_);
 
     return Vector3d(p.x, p.y, p.z) ;
 
@@ -94,6 +104,8 @@ int main(int argc, char **argv) {
 
     grabber.connect() ;
 
+    ros::Duration(1.0).sleep() ;
+
     ros::Time ts ;
     image_geometry::PinholeCameraModel cm ;
 
@@ -102,9 +114,19 @@ int main(int argc, char **argv) {
     cv::Mat clr, depth ;
     grabber.grab(clr, depth, ts, cm) ;
 
-    clr = cv::imread("/home/malasiot/images/clothes/calibration/on_table/cap_3/cap_rgb_000009.png") ;
-    depth = cv::imread("/home/malasiot/images/clothes/calibration/on_table/cap_3/cap_depth_000009.png", -1) ;
+    clr_ = clr ;
+   cv::imwrite("/tmp/grabber0_c.png", clr) ;
+    cv::imwrite("/tmp/grabber0_d.png", depth) ;
 
+ //   clr = cv::imread("/home/malasiot/images/clothes/calibration/on_table/cap_3/cap_rgb_000009.png") ;
+ //   depth = cv::imread("/home/malasiot/images/clothes/calibration/on_table/cap_3/cap_depth_000009.png", -1) ;
+ //   clr = cv::imread("/tmp/grabber0_c.png") ;
+//        depth = cv::imread("/tmp/grabber0_d.png", -1) ;
+
+        cv::Mat gray ;
+        depth.convertTo(gray, CV_8UC1, 0.1) ;
+        cv::imwrite("/tmp/gray.png", gray) ;
+//
     // detect table and object
 
     ObjectOnPlaneDetector objDet(depth, cm.fx(), cm.fy(), cm.cx(), cm.cy()) ;
@@ -119,17 +141,29 @@ int main(int argc, char **argv) {
     cv::Mat dmap ;
     cv::Mat mask = objDet.findObjectMask(n, d, 0.01, dmap, hull) ;
 
+    cv::imwrite("/tmp/mask.png", mask) ;
+
     tf::TransformListener listener ;
     Affine3d frame = getSensorPose(listener, camera) ;
     double tableHeight = getTableHeightFromPlane(n, d, frame) ;
 
     Vector3d t = retargetSensor(dmap, hull, cm, defaultDist - defaultTableHeight) ;
 
-    double newDist = t.z() + defaultTableHeight - gripperOffset ;
+    Vector3d target = frame.inverse() * t ;
+
+    double newDist = target.z() +  0.7 ;
+
+    //double newDist = t.z() + defaultTableHeight - gripperOffset ;
 
     if ( newDist < defaultTableHeight ) return 0 ;
 
-    moveGripperPointingDown(cmove, armName, t.x(), -0.9 + t.y(), newDist );
+    trajectory_msgs::JointTrajectory traj ;
+    if ( robot_helpers::planXtionToPose(armName, Vector3d(target.x(), target.y(), newDist ), robot_helpers::lookAt(Vector3d(0, 0, -1)), traj) )
+    {
+        cmove.execTrajectory(traj) ;
+    }
+
+    //moveGripperPointingDown(cmove, armName, target.x(), target.y(), newDist );
 
     grabber.grab(clr, depth, ts, cm) ;
 
