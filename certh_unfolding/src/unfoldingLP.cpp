69dbba021683704b0defb743feb38f3e30a30db6
akargakos <andreaskargakos@gmail.com>
2013-10-02 17:09:18 +0300

    unfolding with lowest point added

---
+++ certh_unfolding/src/unfoldingLP.cpp
@@ -0,0 +1,33 @@
+#include "RFLibrary/rf.h"
+#include "RFLibrary/rf.cpp"
+#include "HFLibrary/hf.h"
+#include "HFLibrary/hf.cpp"
+#include "Unfold.h"
+#include <sstream>
+#include <time.h>
+#include <camera_helpers/OpenNIServiceClient.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "graspTwoTimesLP");
+    ros::NodeHandle nh;
+
+    ros::Publisher marker_pub;
+    marker_pub = nh.advertise<visualization_msgs::Marker>("/visualization_marker_", 0);
+
+    sleep(3);
+    Unfold uf("r2",marker_pub );
+    while(!uf.graspMiddle());
+    while(!uf.graspLowestPoint());
+
+//    ros::Duration(3).sleep() ;
+
+//    while(uf.graspLowestPoint(false, true));
+//    while(uf.graspLowestPoint(true, true));
+
+    camera_helpers::openni::disconnect(uf.camera) ;
+    setServoPowerOff() ;
+    return 0;
+}
